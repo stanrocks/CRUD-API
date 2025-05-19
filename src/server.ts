d03d3e044853fc969e0907eb96b6b8d77ce9c00e
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2025-05-19 21:57:07 +0500

    feat: add internal server error handling

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,21 @@
 import http from 'node:http';
 
 export const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(
-    JSON.stringify({
-      data: 'Hello World!',
-    }),
-  );
+  try {
+    console.log(`Request received: ${req.method} ${req.url}`);
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(
+      JSON.stringify({
+        data: 'Hello World!',
+      }),
+    );
+  } catch (error) {
+    console.error('Error handling request:', error);
+    res.writeHead(500, { 'Content-Type': 'application/json' });
+    res.end(
+      JSON.stringify({
+        error: 'Internal Server Error',
+      }),
+    );
+  }
 });
